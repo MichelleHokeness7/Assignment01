c8c722f9bb37231bce44d1b4d4291956b5da93b5
Michelle Hokeness <mickeymh@icloud.com>
2022-02-19 21:18:53 -0800

    Initial commit

---
+++ Assignment01/js/script.js
@@ -0,0 +1,54 @@
+// STEP 1 
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction
+
+// STEP 2
+// 50
+// 'Hello world!'
+// true
+// null 
+
+// STEP 3
+let appleBush = (5 + 10) * (20 / 2) + 100;
+let anExpression = 3 * (4 / 5) + 6;
+
+// STEP 4
+
+// STEP 5
+
+// STEP 6
+
+// STEP 7
+
+// STEP 8
+
+// STEP 9
+
+// STEP 10
+
+// STEP 11
+
+// STEP 12
+
+// STEP 13
+
+// STEP 14
+
+// STEP 15
+
+// STEP 16
+
+// STEP 17
+
+// STEP 18
+
+// STEP 19
+
+// STEP 20
+
+// STEP 21
+
+// STEP 22
