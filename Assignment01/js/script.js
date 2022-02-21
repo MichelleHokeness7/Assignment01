0b411c9f8806c7ce93560f8297bcd244d01a5613
Michelle Hokeness <mickeymh@icloud.com>
2022-02-21 13:03:07 -0800

    added steps 5-22

--- Assignment01/js/script.js
+++ Assignment01/js/script.js
@@ -3,21 +3,21 @@
 // function theMonth();
 // let currentMonth;
 // let summerMonth;
-// let myLibraryFunction
+// let myLibraryFunction;
 
 // STEP 2
-// 50
-// 'Hello world!'
-// true
-// null 
+// 50;
+// 'Hello world!';
+// true;
+// null;
 
 // STEP 3
 // let appleBush = (5 + 10) * (20 / 2) + 100;
 // let anExpression = 3 * (4 / 5) + 6;
 
 // STEP 4
-// let firstName;
-// let lastName;
+// const firstName;
+// const lastName;
 // let address;
 // let City;
 // let State;
@@ -26,40 +26,90 @@
 // let referralSource;
 // let mayWeContactYou;
 
-
-
 // STEP 5
+// const firstName = "Michelle";
+// const lastName = "Hokeness";
+// let yourAge = 50;
 
 // STEP 6
+// let myAge = 50;
+// console.log("Michelle Hokeness is " + myAge + " years young.")
 
 // STEP 7
+// isLearning = true;
+// myAge = 50;
+// console.log("Michelle is learning javaScript! " + isLearning);
+// console.log(myAge + isLearning);
 
 // STEP 8
+// No, not valid
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+// alert(someString);
 
 // STEP 9
+// let priceDrop = null;
+// console.log(priceDrop);
+// let priceIncrease;
+// console.log(priceIncrease);
 
 // STEP 10
+// console.log(typeof "Michelle");
+// console.log(typeof 50);
+// console.log(typeof false);
+// console.log(typeof null);
+// console.log(typeof myName);
 
 // STEP 11
+// alert("Hello" + " Michelle Hokeness" + ", welcome to the JavaScript class!");
 
 // STEP 12
+// let name = "Michelle Hokeness";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
 
 // STEP 13
+// let course = "JavaScript";
+// let name = "Michelle Hokeness";
+// alert("Hello " + name + ", welcome to the " + course + " class!");
 
 // STEP 14
+// let course = "JavaScript";
+// let name = "Michelle Hokeness";
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
 
 // STEP 15
+// let name = prompt("What is your name?");
+// console.log(name);
 
 // STEP 16
+// let course = prompt("What class are you taking?");
+// console.log(course);
 
 // STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
 
 // STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
 
 // STEP 19
+// let x = 20;
+// x *=5;
+// console.log(x);
 
 // STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
 
 // STEP 21
+// let x = 10 - 2;
+// let y = 4 * 2;
+// console.log(x == 8 || y == 10);
 
 // STEP 22
+// let x = 10 - 2;
+// let y = 4 * 2;
+// console.log(x == 8 && y == 10);
