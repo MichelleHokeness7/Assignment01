71fb3607f3c31e88d919f8516c6ebac5ec487da5
Michelle Hokeness <mickeymh@icloud.com>
2022-03-01 09:03:10 -0800

    first 3 applications

---
+++ Assignment02/js/script.js
@@ -0,0 +1,45 @@
+// LARGER OR SMALLER?
+// let applesToday = window.prompt("How many apples would you like today?");
+// let applesTomorrow = window.prompt("How many apples would you like tomorrow?");
+
+// if (applesToday > applesTomorrow) {
+//     window.document.write("You chose " + applesToday + " apples today over " + applesTomorrow + " apples tomorrow.");
+// } else if (applesToday < applesTomorrow) {
+//     window.document.write("You chose " + applesTomorrow + " apples tomorrow over " + applesToday + " apples today.");
+// }
+
+// THE WORD TRANSLATOR
+// let languageCode = window.prompt('Enter language code of choice: es, de, en, or fr');
+// if (languageCode === "es") {
+//     window.console.log('Hello World translated in Spanish is: Hola Mundo');
+// } else if (languageCode === "de") {
+//     window.console.log('Hello World translated in German is: Hallo Welt');
+// } else if (languageCode === "en") {
+//     window.console.log('Hello World translated in English is: Hello World');
+// } else if (languageCode === "fr") {
+//     window.console.log('Hello World translated in French is: Bonjour le monde');
+// }else {
+//     window.console.log('Hello World translated in English is: Hello World');
+// }
+
+// THE "COIN FLIP" GAME
+// let randomNum = Math.round(Math.random() * 10);
+// let coinFlip = randomNum;
+
+// let choice = window.prompt('Enter heads or tails').toLowerCase();
+// if (coinFlip <= 5) {
+//     if (coinFlip <= 5 && choice === "heads") {
+//         alert('The flip was heads and you chose heads... you win!');
+//     } else if (coinFlip <= 5 && choice === "tails") {
+//         alert('The flip was heads but you chose tails... you lose!');
+//     }
+// } else if (coinFlip > 5) {
+//     if (coinFlip > 5 && choice === "heads") {
+//         alert('The flip was tails but you chose heads... you lose!');
+//     } else if (coinFlip > 5 && choice === "tails") {
+//         alert('The flip was tails and you chose tails... you win!');
+//     }
+// }
+
+// console.log(randomNum);
+ 
