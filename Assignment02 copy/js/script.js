3f81c7cc597427479fb291667da35bf176d2b9dc
Michelle Hokeness <mickeymh@icloud.com>
2022-03-03 07:32:53 -0800

    udated Hitchhiker's guide game

--- Assignment02 copy/js/script.js
+++ Assignment02 copy/js/script.js
@@ -62,33 +62,33 @@
 //     } 
 
 // THE "HITCHHIKER'S GUIDE TO THE GALAXY" GAME
-// let userResponse = window.prompt('Hello friend. Are you ready to play this game, yes/no?').toLowerCase();
-// if (userResponse === "yes") {
-//     alert('You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.');
+let userResponse = window.prompt('Hello friend. Are you ready to play this game, yes/no?').toLowerCase();
+if (userResponse === "yes") {
+    alert('You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.');
 
-// let userDirection = window.prompt('Which direction would you like to head to (please enter forward, left, or right)?');
-// switch (userDirection) {
-//     case "left":
-//         alert('Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown!');
-//         break;
-//     case "forward":
-//         alert('You walk 100 yards and safely make your way out of the cave.');
-//         break;
-//     case "right":
-//         alert('You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!');
-//         break;
-//     default:
-//         alert('The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.');
-// }
-// let userRating = parseInt(window.prompt('Please rate this game using a scale that is between 1 and 10'));
-// if (userRating < 1 || userRating > 10) {
-//     alert('Thank you for playing the game.');
-// } else if (userRating >= 6 && userRating <= 10) {
-//     alert('Thank you for playing. Want to play again? if so, hit refresh.');
-// } else if (userRating >= 1 && userRating <= 5) {
-//     alert('Thank you for playing. I am working hard to improve the game.');
-// }
+let userDirection = window.prompt('Which direction would you like to head to (please enter forward, left, or right)?');
+switch (userDirection) {
+    case "left":
+        alert('Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown!');
+        break;
+    case "forward":
+        alert('You walk 100 yards and safely make your way out of the cave.');
+        break;
+    case "right":
+        alert('You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!');
+        break;
+    default:
+        alert('The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.');
+}
+let userRating = parseInt(window.prompt('Please rate this game using a scale that is between 1 and 10'));
+if (userRating < 1 || userRating > 10) {
+    alert('Thank you for playing the game.');
+} else if (userRating >= 6 && userRating <= 10) {
+    alert('Thank you for playing. Want to play again? if so, hit refresh.');
+} else if (userRating >= 1 && userRating <= 5) {
+    alert('Thank you for playing. I am working hard to improve the game.');
+}
 
-// } else if (userResponse === 'no' || userResponse !== 'yes') {
-//     alert('Thank you and please come back to play another time.');
-// }
+} else if (userResponse === 'no' || userResponse !== 'yes') {
+    alert('Thank you and please come back to play another time.');
+}
