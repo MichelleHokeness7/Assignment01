c9d904f3e79cd1f2014486fd6383c1c9afde46b3
Michelle Hokeness <mickeymh@icloud.com>
2022-04-04 12:49:34 -0700

    window.open to finally work in add-employee.js

--- assignment06/beginning/js/add-employee.js
+++ assignment06/beginning/js/add-employee.js
@@ -1,28 +1,45 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
-// window.addEventListener('load', () => {
-document.getElementById('btnAddEmployee');
+ window.addEventListener('load', () => {
+// document.getElementById('btnAddEmployee');
 
     // SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
-    window.open('add-employee.html', 'Add Employee Form', 'width=800, height=700');
-// });
+    // RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
+    // MOVE THE POPUP TO THE MIDDLE OF THE SCREEN
+        window.open('add-employee.html', 'Add Employee Form', 'width=800, height=700, top=400, left=400');
+ 
+});
 
 
 
-// RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
 
-// MOVE THE POPUP TO THE MIDDLE OF THE SCREEN
-// window.moveTo(0, 0);
+
+
 
 // CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM
+const $ = (id) => document.getElementById(id);
 
 // HANDLE THE CANCEL BUTTON. WHEN THE USER CLICKS THIS BUTTON, CLOSE THE WINDOW
+let cancelBtn = document.getElementById('cancel').addEventListener('click', () => {
+    
+    window.close(cancelBtn);
+    
+});
 
 // HANDLE THE SUBMISSION OF THE FORM AND THEN IMMEDIATELY PREVENT THE SUBMISSION
 
+
 // CREATE 5 VARIABLES FOR ID, NAME, EXT, EMAIL, AND DEPT
+let empId;
+let fullName;
+let ext;
+let emailAddress;
+let department;
+
 // SET THOSE VARIABLES TO WHATEVER THE USER ENTERS INTO THE FORM ELEMENTS
 
+
 // GET THE LOGINDETAILS OUTPUT ELEMENT FROM THE PARENT PAGE
+let output;
 
 // SET THE TEXT OF THE LOGINDETAILS ELEMENT TO THE ABOVE SET VARIABLES
 
