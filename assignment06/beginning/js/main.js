d7db3f20a0a26a6f64d684ec136278147054c5fb
Michelle Hokeness <mickeymh@icloud.com>
2022-04-04 12:51:01 -0700

    window.open to work in add employee.js

--- assignment06/beginning/js/main.js
+++ assignment06/beginning/js/main.js
@@ -1,14 +1,14 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
 window.addEventListener('load', init);
 function init() {
-    event.preventDefault();
 // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
 
-    document.getElementById('btnAddEmployee').addEventListener('click', () => {
+    
+document.getElementById('btnAddEmployee').addEventListener('click', () => {
     
         // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
-        window.open('add-employee.html', 'Add Employee Form', 'width=800, height=700');
-        window.moveTo(0, 0);
+        window.open('add-employee.html', 'Add Employee Form', 'width=800, height=700, top=400, left=400');
+        
     })
 
 }
