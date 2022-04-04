9352954fcecbfb4b27f4c05af963eb4f7ac1bbe6
Michelle Hokeness <mickeymh@icloud.com>
2022-04-04 16:34:58 -0700

    added output.innerHTML values

--- assignment06/beginning/js/add-employee.js
+++ assignment06/beginning/js/add-employee.js
@@ -1,5 +1,6 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
- window.addEventListener('load', () => {
+ window.addEventListener('load', (event) => {
+     event.preventDefault();
 // document.getElementById('btnAddEmployee');
 
     // SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
@@ -8,12 +9,6 @@
         window.open('add-employee.html', 'Add Employee Form', 'width=800, height=700, top=400, left=400');
  
 });
-
-
-
-
-
-
 
 // CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM
 const $ = (id) => document.getElementById(id);
@@ -29,17 +24,56 @@ let cancelBtn = document.getElementById('cancel').addEventListener('click', () =
 
 
 // CREATE 5 VARIABLES FOR ID, NAME, EXT, EMAIL, AND DEPT
-let empId;
+let employeeNum;
 let fullName;
 let ext;
 let emailAddress;
 let department;
+let submit;
+let output;
 
 // SET THOSE VARIABLES TO WHATEVER THE USER ENTERS INTO THE FORM ELEMENTS
+employeeNum = $('id');
+fullName = $('name');
+ext = $('extension');
+emailAddress = $('email');
+department = $('department');
+submit = $('submit');
+output = $('loginDetails');
+
+
 
 
 // GET THE LOGINDETAILS OUTPUT ELEMENT FROM THE PARENT PAGE
-let output;
+// window.addEventListener('load', () => {
+    
+    document.getElementById('submit').addEventListener('click', (event) => {
+        event.preventDefault();
+        let formElement = document.getElementById('empForm');
+
+        formElement.addEventListener('submit', writeMessage);
+
+        function writeMessage(event) {
+            event.preventDefault();
+            window.console.log('Hello');
+            output = window.opener.document.getElementById('loginDetails');
+            output.innerHTML = `ID: ${employeeNum.value}`;
+            output.innerHTML = `Name: ${fullName.value}`;
+            output.innerHTML = `Extension: ${ext.value}`;
+            output.innerHTML = `Email: ${emailAddress.value}`;
+            output.innerHTML = `Department: ${department.value}`;
+            console.log('hello');
+        }
+        window.close();
+        
+    
+        
+    
+    })
+
+    
+// })
+
 
 // SET THE TEXT OF THE LOGINDETAILS ELEMENT TO THE ABOVE SET VARIABLES
 
