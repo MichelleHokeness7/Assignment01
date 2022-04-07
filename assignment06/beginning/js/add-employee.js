a8759c63f06cbefa4d4e7b5bef5aa32c3c2463c5
Michelle Hokeness <mickeymh@icloud.com>
2022-04-06 17:03:10 -0700

    I dunno

--- assignment06/beginning/js/add-employee.js
+++ assignment06/beginning/js/add-employee.js
@@ -1,26 +1,39 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
- window.addEventListener('load', (event) => {
-     event.preventDefault();
+ window.addEventListener('load', init);
+    function init() {
+   
 // document.getElementById('btnAddEmployee');
 
     // SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
     // RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
     // MOVE THE POPUP TO THE MIDDLE OF THE SCREEN
-        window.open('add-employee.html', 'Add Employee Form', 'width=800, height=700, top=400, left=400');
+     let overrideIndex = window.open('add-employee.html', 'AddEmployeeForm', 'width=800, height=700, top=400, left=400');
  
-});
 
+    };
 // CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM
-const $ = (id) => document.getElementById(id);
+    const $ = (id) => document.getElementById(id);
 
 // HANDLE THE CANCEL BUTTON. WHEN THE USER CLICKS THIS BUTTON, CLOSE THE WINDOW
-let cancelBtn = document.getElementById('cancel').addEventListener('click', () => {
+    let cancelBtn = document.getElementById('cancel').addEventListener('click', () => {
+    cancelBtn.close();
     
-    window.close(cancelBtn);
+    
     
 });
 
 // HANDLE THE SUBMISSION OF THE FORM AND THEN IMMEDIATELY PREVENT THE SUBMISSION
+// let formElement = document.getElementById('submit');
+// formElement.addEventListener('click', empInfo());
+// function empInfo(e) {
+//     e.preventDefault();
+// };
+
+let formElement = document.getElementById('submit').addEventListener('click', (e) => {
+    e.preventDefault();
+
+})
+
 
 
 // CREATE 5 VARIABLES FOR ID, NAME, EXT, EMAIL, AND DEPT
@@ -29,8 +42,8 @@ let fullName;
 let ext;
 let emailAddress;
 let department;
-let submit;
-let output;
+// let submit;
+// let output;
 
 // SET THOSE VARIABLES TO WHATEVER THE USER ENTERS INTO THE FORM ELEMENTS
 employeeNum = $('id');
@@ -38,45 +51,29 @@ fullName = $('name');
 ext = $('extension');
 emailAddress = $('email');
 department = $('department');
-submit = $('submit');
+// submit = $('submit');
 output = $('loginDetails');
-
+let formElement = $('submit');
 
 
 
 // GET THE LOGINDETAILS OUTPUT ELEMENT FROM THE PARENT PAGE
-// window.addEventListener('load', () => {
-    
-    document.getElementById('submit').addEventListener('click', (event) => {
-        event.preventDefault();
-        let formElement = document.getElementById('empForm');
-
-        formElement.addEventListener('submit', writeMessage);
-
-        function writeMessage(event) {
-            event.preventDefault();
-            window.console.log('Hello');
-            output = window.opener.document.getElementById('loginDetails');
-            output.innerHTML = `ID: ${employeeNum.value}`;
-            output.innerHTML = `Name: ${fullName.value}`;
-            output.innerHTML = `Extension: ${ext.value}`;
-            output.innerHTML = `Email: ${emailAddress.value}`;
-            output.innerHTML = `Department: ${department.value}`;
-            console.log('hello');
-        }
-        window.close();
-        
-    
-        
-    
-    })
-
-    
-// })
+let formElement = window.open('index.html', 'empInfoForm');
 
 
 // SET THE TEXT OF THE LOGINDETAILS ELEMENT TO THE ABOVE SET VARIABLES
+window.addEventListener('load', () => {
+    let output = window.opener.document.getElementById('loginDetails');
+    output.innerHTML = `ID: ${employeeNum}`;
+    output.innerHTML = `Name: ${fullName}`;
+    output.innerHTML = `Extension: ${ext}`;
+    output.innerHTML = `Email: ${emailAddress}`;
+    output.innerHTML = `Department: ${department}`;
+
+})
 
 // CLOSE THE POPUP
+
+overrideIndex.close();
 
 // THE DATA SHOULD SHOW ON THE INDEX.HTML PAGE
--- assignment06/beginning/js/main.js
+++ assignment06/beginning/js/main.js
@@ -7,9 +7,10 @@ function init() {
 document.getElementById('btnAddEmployee').addEventListener('click', () => {
     
         // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
-        window.open('add-employee.html', 'Add Employee Form', 'width=800, height=700, top=400, left=400');
+        window.open('add-employee.html', 'AddEmployeeForm', 'width=800, height=700, top=400, left=400');
         
     })
 
 }
+
 
