6f0d926516b8f10934756d44d8bdd9820e5235da
Michelle Hokeness <mickeymh@icloud.com>
2022-05-04 16:30:10 -0700

    initial commit and adding string methods

---
+++ assignment10/beginning/js/strings.js
@@ -0,0 +1,43 @@
+//STEP 1
+// let userName = prompt('Please enter your name.').toLowerCase();
+// alert(userName.length);
+//STEP 2
+// let userName = prompt('Please enter your name.').toLowerCase();
+// let userNumber = prompt('Pick a number between 1 - whatever number that represents a letter in your name minus 1');
+// alert(userName.charAt(userNumber));
+
+//STEP 3
+// let fistName = prompt('Enter your first name');
+// let lastName = prompt('Enter your last name');
+// alert(`Your name is: ${fistName.concat(lastName)}`)
+
+//STEP 4
+// let theSaying = 'The quick brown fox jumps over the lazy dog';
+// alert(theSaying.indexOf('fox'));
+
+//STEP 5
+// let theSaying = 'The quick brown fox jumps over the lazy fox';
+// alert(theSaying.lastIndexOf('fox'));
+
+//STEP 6
+// let theSaying = 'The quick brown fox jumped over the lazy dog';
+// let fullName = prompt('Please enter your full name.');
+// alert(theSaying.replace('the lazy dog', fullName));
+
+//STEP 7
+// let theSaying = 'The quick brown fox jumps over the lazy dog';
+// let userInput = prompt('Pick a word from - "The quick brown fox jumps over the lazy dog".');
+// alert(theSaying.search(userInput));
+
+//STEP 8
+// let old_string = 'The quick brown fox jumps over the lazy dog';
+// let new_string = old_string.slice(31, 43);
+// alert(new_string.toUpperCase());
+
+//STEP 9
+// let theSaying = '     THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG     ';
+// alert(theSaying.trim().toLowerCase());
+
+//STEP 10
+// let theSaying = 'the quick brown fox jumps over the lazy dog';
+// alert(theSaying.replace('the', 'The'));
