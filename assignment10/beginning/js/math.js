adb73a988af45c51b867b49f213c6b9237151596
Michelle Hokeness <mickeymh@icloud.com>
2022-05-04 18:06:25 -0700

    added step 4

--- assignment10/beginning/js/math.js
+++ assignment10/beginning/js/math.js
@@ -1,9 +1,18 @@
 //STEP 1
+// let userInput = prompt('Enter a number');
+// console.log(Math.abs(userInput));
 
 //STEP 2
+// let userInput = prompt('Please enter a decimal number');
+// console.log(Math.round(userInput));
 
 //STEP 3
+// let userInput = prompt('Please enter a decimal number');
+// console.log(Math.floor(userInput));
 
 //STEP 4
+// let userInput = parseInt(prompt('Please enter a five numbers and use commas to separate them.'));
+// console.log(Math.max(userInput));
+// console.log(Math.min(userInput));
 
 //STEP 5
